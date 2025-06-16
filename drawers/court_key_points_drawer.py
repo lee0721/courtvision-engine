e9fa1015072ec5d19eae86943ecc0b9f2693d3b8
lee0721 <92746404+lee0721@users.noreply.github.com>
2025-06-16 15:55:34 +0100

    改為使用圓形標示 court keypoints

--- drawers/court_key_points_drawer.py
+++ drawers/court_key_points_drawer.py
@@ -24,13 +24,13 @@ class CourtKeypointDrawer:
         """
         vertex_annotator = sv.VertexAnnotator(
             color=sv.Color.from_hex(self.keypoint_color),
-            radius=20)
+            radius=20, thickness=2)
         
         vertex_label_annotator = sv.VertexLabelAnnotator(
             color=sv.Color.from_hex(self.keypoint_color),
             text_color=sv.Color.WHITE,
             text_scale=0.4,
-            text_thickness=1
+            text_thickness=2
         )
         
         output_frames = []
