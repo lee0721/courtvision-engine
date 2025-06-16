0ca6a51dd2d44cfdb1967275c1d46853582d6ceb
lee0721 <92746404+lee0721@users.noreply.github.com>
2025-06-16 16:12:30 +0100

    改為使用圓形標示 court keypoints

--- drawers/court_key_points_drawer.py
+++ drawers/court_key_points_drawer.py
@@ -22,10 +22,11 @@ class CourtKeypointDrawer:
         Returns:
             list: A list of frames with keypoints drawn on them.
         """
+        """
         vertex_annotator = sv.VertexAnnotator(
             color=sv.Color.from_hex(self.keypoint_color),
             radius=25)
-        
+        """
         vertex_label_annotator = sv.VertexLabelAnnotator(
             color=sv.Color.from_hex(self.keypoint_color),
             text_color=sv.Color.WHITE,
@@ -39,9 +40,11 @@ class CourtKeypointDrawer:
 
             keypoints = court_keypoints[index]
             # Draw dots
+            """
             annotated_frame = vertex_annotator.annotate(
                 scene=annotated_frame,
                 key_points=keypoints)
+            """
             # Draw labels
             # Convert PyTorch tensor to numpy array
             keypoints_numpy = keypoints.cpu().numpy()
