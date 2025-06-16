174dbcbf45d7063d3df15aede4743f6aafb428bd
lee0721 <92746404+lee0721@users.noreply.github.com>
2025-06-16 17:01:28 +0100

    modify tactical_view_drawer x y

--- drawers/tactical_view_drawer.py
+++ drawers/tactical_view_drawer.py
@@ -2,8 +2,8 @@ import cv2
 
 class TacticalViewDrawer:
     def __init__(self, team_1_color=[255, 245, 238], team_2_color=[128, 0, 0]):
-        self.start_x = 50
-        self.start_y = 90
+        self.start_x = 150
+        self.start_y = 190
         self.team_1_color = team_1_color
         self.team_2_color = team_2_color
 
