7eaa5213e4af018ed92f3c77519fec2f72112db6
lee0721 <92746404+lee0721@users.noreply.github.com>
2025-06-16 13:04:37 +0100

    update team&pass drawer text update depands on background modify team_assigner test

--- drawers/team_ball_control_drawer.py
+++ drawers/team_ball_control_drawer.py
@@ -70,7 +70,7 @@ class TeamBallControlDrawer:
         box_height = int(frame_height * 0.10)
         margin = 30
 
-        rect_x2 = frame_width - margin - 200
+        rect_x2 = frame_width - margin - 500
         rect_x1 = rect_x2 - box_width
         rect_y2 = frame_height - margin
         rect_y1 = rect_y2 - box_height
