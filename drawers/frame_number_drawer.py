1ee5ea53d84aa99d834af6a029a76b5b56846bcd
lee0721 <92746404+lee0721@users.noreply.github.com>
2025-06-17 11:43:27 +0100

    modify frame_number_drawer

--- drawers/frame_number_drawer.py
+++ drawers/frame_number_drawer.py
@@ -18,12 +18,12 @@ class FrameNumberDrawer:
             timer_text = f"{minutes:02d}:{seconds:02d}.{milliseconds:02d}"
 
             # 畫白底黑字的框框
-            text_size = cv2.getTextSize(time_text, cv2.FONT_HERSHEY_DUPLEX, 1, 2)[0]
+            text_size = cv2.getTextSize(timer_text, cv2.FONT_HERSHEY_DUPLEX, 1, 2)[0]
             text_x = 50
             text_y = frame.shape[0] - 50
             cv2.rectangle(frame, (text_x - 5, text_y - text_size[1] - 5),
                           (text_x + text_size[0] + 5, text_y + 5), (255, 255, 255), -1)
-            cv2.putText(frame, time_text, (text_x, text_y),
+            cv2.putText(frame, timer_text, (text_x, text_y),
                         cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 2)
 
             output_frames.append(frame)
